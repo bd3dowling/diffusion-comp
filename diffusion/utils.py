6da57836af64f0c7446b395b48c4ecae8b30ec63
Ben Dowling <ben.dowling@icloud.com>
2023-12-03 13:06:55 +0000

    test: package installation in colab

---
+++ diffusion/utils.py
@@ -0,0 +1,10 @@
+def plus_one(x: int) -> int:
+    """Adds 1 to an integer
+
+    Args:
+        x (int): some integer
+
+    Returns:
+        int: passed integer plus one
+    """
+    return x + 1
